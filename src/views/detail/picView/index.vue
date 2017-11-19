e902cbd87fe4a84c64cb4adeaa9eb4485726e313
heungm <liuwh@qiangqiaoyi.com>
2017-11-20 00:13:57 +0800

    CHG 图片流畅滑动

--- src/views/detail/picView/index.vue
+++ src/views/detail/picView/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div :class="$style.main" :style="style">
-    <div :class="$style.item" v-for="(pic,index) in picList" :key="index" @click="preview" @animationend="animationend($event, pic, index)">
-      <img :src="pic.url">
+  <div :class="$style.main" :style="style" ref="main">
+    <div :class="$style.item"
+    v-for="(pic,index) in picList" :key="index" ref="items">
+      <div :class="$style.imgDiv">
+        <img :src="pic.url" @click="clickImg(pic.url, index)">
+      </div>
     </div>
+    <img :src="activePic.url" :class="$style.animat"
+    v-if="activePic && activePic.url" @animationend="animationend">
     <div :class="$style.mask" v-if="previewData.show" @click="closeMask">
-      <!-- <img :src="previewData.src"> -->
       <swiper :class="$style.containerClass" :options="option" ref="swiper">
         <swiperSlide v-for="(pic,index) in picList" :key="index">
           <img :src="pic.url">
@@ -25,15 +29,16 @@ export default {
     return {
       style: null,
       previewData: {
-        show: false,
-        src: ''
+        show: false
       },
+      activePic: null,
       picList: [],
       lockPreview: false,
       option: {}
     }
   },
   created () {
+    this.$store.dispatch(Types.OPEN_LOADING)
     this.style = {
       'min-height': window.innerHeight + 'px'
     }
@@ -47,28 +52,26 @@ export default {
       if (data && data.length) {
         this.picList = data
         this.$store.dispatch(Types.CHANGE_NAV, { title: `更多图片 Fit-map` })
-        this.$store.dispatch(Types.CLOSE_LOADING)
+        this.$nextTick(() => {
+          setTimeout(() => {
+            this.$store.dispatch(Types.CLOSE_LOADING)
+          }, 500)
+          this.initImgs()
+        })
       } else {
         this.$router.back()
       }
     })
   },
+  mounted () {
+  },
   methods: {
-    preview (e) {
-      if (this.lockPreview) {
-        return
-      }
-      const el = e.path[1]
-      el.className += ' ' + this.$style.animat
-      this.lockPreview = true
-    },
     animationend (e, pic, index) {
-      this.option.initialSlide = index
+      this.option.initialSlide = this.activePic.index
+      this.activePic = null
       Object.assign(this.previewData, {
-        show: true,
-        src: pic
+        show: true
       })
-      e.target.className = this.$style.item
       this.lockPreview = false
     },
     closeMask () {
@@ -76,14 +79,61 @@ export default {
         show: false,
         src: ''
       }
+    },
+    clickImg (url, index) {
+      this.activePic = {
+        url: url,
+        index: index
+      }
+    },
+    initImgs () {
+      const list = this.$refs.items
+      const itemW = list[0].offsetWidth
+      const itemH = []
+      const length = list.length
+      setPosition(list, 0, itemH, length, itemW)
     }
   }
 }
+function setPosition (list, i, itemH, length, itemW) {
+  if (i >= length) {
+    return
+  }
+  if (i < 2) {
+    const Img = list[i].children[0].children[0]
+    if (Img && Img.height) {
+      itemH.push(list[i].offsetHeight)
+      setPosition(list, i + 1, itemH, length, itemW)
+    } else {
+      Img.onload = () => {
+        itemH.push(list[i].offsetHeight)
+        setPosition(list, i + 1, itemH, length, itemW)
+      }
+    }
+  } else {
+    const Img = list[i - 2].children[0].children[0]
+    const index = i % 2
+    if (Img && Img.width > 50) {
+      list[i].style.cssText = `;position:absolute;left:${itemW * index}px;top:${itemH[index]}px`
+      // 这一步很重要，防止剩下图片全都定位在同一坐标
+      itemH[index] += list[i].offsetHeight
+      setPosition(list, i + 1, itemH, length, itemW)
+    } else {
+      Img.onload = () => {
+        list[i].style.cssText = `;position:absolute;left:${itemW * index}px;top:${itemH[index]}px`
+        // 这一步很重要，防止剩下图片全都定位在同一坐标
+        itemH[index] += list[i].offsetHeight
+        setPosition(list, i + 1, itemH, length, itemW)
+      }
+    }
+  }
+}
+
 </script>
 
 <style lang="stylus" module>
 @import '~tool/vendor'
-
+/*
 .main
   column-count 2
   column-gap (6/20)rem
@@ -100,6 +150,22 @@ export default {
   >img
     display block
     width 100%
+*/
+.main
+  position relative
+
+.item
+  float left
+  padding 6px 0 0 (6/20)rem
+
+.imgDiv
+  width (178.5/20)rem
+  overflow hidden
+  img
+    padding 0
+    display block
+    width 100%
+
 
 @keyframes preview
   from
@@ -118,7 +184,7 @@ export default {
     z-index 10
 
 .animat
-  animation preview 1s
+  animation preview .5s
 
 .mask
   position fixed
