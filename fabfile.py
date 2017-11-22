40bcd7fe02a709ccd8565c3ab006415b2fb3e87e
heungm <liuwh@qiangqiaoyi.com>
2017-11-22 23:40:53 +0800

    CHG fab

---
+++ fabfile.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#coding:utf-8
+from fabric.api import run, env, local, put
+import sys
+import os
+print sys.path
+sys.path.append('./build')
+
+env.hosts = ['fitmap@120.24.154.176']
+
+def hello():
+  print("Hello world!")
+
+def hello2(name):
+  if isinstance(name, str):
+    print("Hello %s!" % name)
+
+
+def prod(time):
+  if isinstance(time, str):
+    os.system('yarn run build-pro-static -- --define process.env.dwfversion="\'%s\'"' % time)
+    os.system("rename 's/dist\/vendor\..+.js/dist\/vendor\.js/s' dist/vendor.*.js")
+
+def testPro():
+  os.system('yarn run build-pro')
+  removeTestEnv()
+  uploadTestProEnv()
+
+def test():
+  os.system('yarn run build-test')
+  uploadTestEnv()
+
+def removeTestEnv():
+  run('rm -rf /*')
+
+def uploadTestEnv():
+  put('./dist/*', '/home')
+
+def uploadTestProEnv():
+  put('./static/*', '/home/dev/h5/admin')
