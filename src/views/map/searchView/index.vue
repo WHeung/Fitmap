5e70e4749fde45c2c72137b933455fb564144661
heungm <liuwh@qiangqiaoyi.com>
2017-11-26 23:57:10 +0800

    CHG QMap

--- src/views/map/searchView/index.vue
+++ src/views/map/searchView/index.vue
@@ -88,8 +88,7 @@ export default {
       this.$store.dispatch(Types.UPDATE_MAP_SEARCH, data).then((list) => {
         if (list && list.length) {
           const itemId = list[0].location_obj.id
-          // const markers = this.map.getAllOverlays('marker')
-          // this.map.setFitView(markers)
+          const markers = this.map.markers
           const marker = markers.find(item => {
             return item.itemId === itemId
           })
